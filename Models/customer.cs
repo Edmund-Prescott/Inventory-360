a805b3e7d026ac547994b4258b6ac7229efd344f
andrew-kotan <96087746+andrew-kotan@users.noreply.github.com>
2022-11-14 18:36:13 -0800

    Initial Commit

---
+++ Models/customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace instrumentRentals2.Models
+{
+    public class customer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; } = "";
+        public string lastName { get; set; } = "";
+    }
+}
+
