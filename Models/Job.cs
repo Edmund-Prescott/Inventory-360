2912050c273b37cb74d78b9f767e5a166b70fd99
SSA-Vale <114950732+SSA-Vale@users.noreply.github.com>
2022-11-22 19:37:53 -0800

    Added models
    models made

---
+++ Models/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory_360.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string JobNumber { get; set; }
+        public int StartDate { get; set; }
+        public int FinishDate { get; set; }
+        public Equipment Equpiment { get; set; }
+    }
+}
