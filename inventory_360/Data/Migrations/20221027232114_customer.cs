d3e00fac8eb5a11f506a14fed007595758161946
andrew-kotan <andrew.kotan@icloud.com>
2022-11-25 20:51:32 -0800

    Project-Redo
    ReCreate of Project to Fix Startup.cs and Program.cs
    Copied Model.cs Files Created by Jordan
    Added ApplicationDbContext Service to Startup.cs
    Added Models to ApplicationDbContext.cs
    Renamed all Namespace to inventory_360

---
+++ inventory_360/Data/Migrations/20221027232114_customer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace inventory_360.Data.Migrations
+{
+    public partial class customer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    firstName = table.Column<string>(nullable: true),
+                    lastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "instrument",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(nullable: true),
+                    type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_instrument", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rentalAgreement",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    rentalStart = table.Column<DateTime>(nullable: false),
+                    instrumentid = table.Column<int>(nullable: true),
+                    customerid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rentalAgreement", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_rentalAgreement_customer_customerid",
+                        column: x => x.customerid,
+                        principalTable: "customer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_rentalAgreement_instrument_instrumentid",
+                        column: x => x.instrumentid,
+                        principalTable: "instrument",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rentalAgreement_customerid",
+                table: "rentalAgreement",
+                column: "customerid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rentalAgreement_instrumentid",
+                table: "rentalAgreement",
+                column: "instrumentid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "rentalAgreement");
+
+            migrationBuilder.DropTable(
+                name: "customer");
+
+            migrationBuilder.DropTable(
+                name: "instrument");
+        }
+    }
+}
---
+++ inventory_360/Models/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventory_360.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string JobNumber { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public Equipment Equpiment { get; set; }
+    }
+}
