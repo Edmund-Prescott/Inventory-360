36040126fce5019866fa00a1b83a22a61522fe55
andrew-kotan <andrew.kotan@icloud.com>
2022-12-07 16:02:27 -0800

    Adding Public API for each Data Class
    Added Client API Controller
    Added Employee API Controller
    Added Equipment API Controller

---
+++ inventory_360/Api/ClientController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using inventory_360.Data;
+using inventory_360.Models;
+
+namespace inventory_360.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/client
+        [HttpGet]
+        public async Task<ActionResult<int>> GetClient()
+        {
+            List<Client> clients = await _context.client.ToListAsync();
+            return clients.Count;
+        }
+
+        // GET: api/Client/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            var client = await _context.client.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        // PUT: api/Client/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(int id, Client client)
+        {
+            if (id != client.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/client
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            _context.client.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = client.Id }, client);
+        }
+
+        // DELETE: api/client/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var client = await _context.client.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.client.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientExists(int id)
+        {
+            return _context.client.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ inventory_360/Api/EquipmentController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using inventory_360.Data;
+using inventory_360.Models;
+
+namespace inventory_360.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EquipmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/equipment
+        [HttpGet]
+        public async Task<ActionResult<int>> Getequipment()
+        {
+            List<Equipment> equipment = await _context.equipment.ToListAsync();
+            return equipment.Count;
+        }
+
+        // GET: api/equipment/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Equipment>> GetEquipment(int id)
+        {
+            var equipment = await _context.equipment.FindAsync(id);
+
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return equipment;
+        }
+
+        // PUT: api/equipment/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEquipment(int id, Equipment equipment)
+        {
+            if (id != equipment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(equipment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EquipmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/equipment
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Equipment>> PostClient(Equipment equipment)
+        {
+            _context.equipment.Add(equipment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEquipment", new { id = equipment.Id }, equipment);
+        }
+
+        // DELETE: api/equipment/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var equipment = await _context.equipment.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            _context.equipment.Remove(equipment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EquipmentExists(int id)
+        {
+            return _context.equipment.Any(e => e.Id == id);
+        }
+    }
+}
